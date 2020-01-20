271bd8031c60caff9ac81a4e0f8a88417abde588
Daouki <daouki97@gmail.com>
2020-01-20 14:07:42 +0100

    Setup project files.

---
+++ Yung/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace Yung
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"yung v{Assembly.GetEntryAssembly()?.GetName().Version} {Environment.NewLine}");
+        }
+    }
+}
